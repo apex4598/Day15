9fcfb9e6a0b2333472b29201dc19008893f074f8
Apex4598 <login2ajit@gmail.com>
2022-08-29 07:51:26 +0530

    Hash Table Binary Tree

---
+++ MyTreeNode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.binarytree;
+
+public class MyTreeNode<K> {
+    K key;
+    MyTreeNode<K> left;
+    MyTreeNode<K> right;
+
+    public MyTreeNode(K key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
